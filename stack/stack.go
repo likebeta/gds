2a0d221313dd505c70b0b1c301b9de62fae4d606
likebeta <ixxoo.me@gmail.com>
2020-05-02 15:53:09 +0800

    array stack

---
+++ stack/stack.go
@@ -0,0 +1,34 @@
+package stack
+
+type ArrayStack struct {
+    arr []interface{}
+}
+
+func (s *ArrayStack) Size() int {
+    return len(s.arr)
+}
+
+func (s *ArrayStack) Empty() bool {
+    return len(s.arr) == 0
+}
+
+func (s *ArrayStack) Push(v interface{}) *ArrayStack {
+    s.arr = append(s.arr, v)
+    return s
+}
+
+func (s *ArrayStack) Pop() interface{} {
+    sz := len(s.arr)
+    v := s.arr[sz-1]
+    s.arr = s.arr[:sz-1]
+    return v
+}
+
+func (s *ArrayStack) Top() interface{} {
+    sz := len(s.arr)
+    return s.arr[sz-1]
+}
+
+func (s *ArrayStack) Peek() interface{} {
+    return s.Top()
+}
